603d9a0ddd144d7d5855c358d5935af4475ea139
florinrusu96 <rusuflorinandrei96@gmail.com>
2019-05-24 18:13:46 +0300

    First commit on blog posts page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BlogPageComponent} from "./blog-page/blog-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blog-page', component: BlogPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { BlogPostComponent } from './blog-page/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    NavigationComponent
+    NavigationComponent,
+    BlogPageComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
