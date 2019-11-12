34de91f30a4c2e9fd0dbee9fdccd715024557d07
florinrusu96 <rusuflorinandrei96@gmail.com>
2019-11-12 23:11:02 +0200

    Made the banner for each page change, changed the way events work for future implementation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { EventPageComponent } from './event-page/event-page.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { StoryPageComponent } from './story-page/story-page.component';
 import { PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import { EventsPageComponent } from './events-page/events-page.component';
 
 const routes: Routes = [
   {path: 'blog-page', component: BlogPageComponent},
   {path: 'home-page', component: HomePageComponent},
+  {path: 'event-page', component: EventsPageComponent},
   {path: 'events/:id', component: EventPageComponent},
   {path: 'contact-form', component: ContactFormComponent},
   {path: 'story-page', component: StoryPageComponent},
