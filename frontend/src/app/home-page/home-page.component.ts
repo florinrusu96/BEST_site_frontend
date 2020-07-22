f1b8b019ec94ff6233bc979602426f54928faf7f
Vlad Vitan <vlad.vitan@stud.acs.upb.ro>
2020-07-23 02:06:59 +0300

    Adding responsive alignment for home page content.

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  // mediaSubscription: Subscription;
+  // columsNo: number = 2;
+  // deviceXs: boolean;
+  breakpoint: number = 2;
+  maxWidth: number = 600;
 
-  constructor() { }
+  constructor(
+    public mediaObserver: MediaObserver
+  ) { }
 
   ngOnInit() {
+    // this.mediaSubscription = this.mediaObserver.media$.subscribe((result: MediaChange) => {
+    //   this.deviceXs = result.mqAlias === 'xs' ? true : false;
+    //   if (this.deviceXs) {
+    //     this.columsNo = 1;
+    //   } else {
+    //     this.columsNo = 2;
+    //   }
+    // });
+    this.breakpoint = (window.innerWidth <= this.maxWidth) ? 1 : 2;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= this.maxWidth ? 1 : 2);
+  }
+
+  ngOnDestroy() {
+    // this.mediaSubscription.unsubscribe();
   }
 
 }
